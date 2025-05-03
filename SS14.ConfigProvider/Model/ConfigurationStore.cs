68bf86274d72090b715088f2c42fab4d84a63fb6
Simon <63975668+Simyon264@users.noreply.github.com>
2025-05-03 08:50:23 +0200

    fix: UpdatedOn Column not having a value.

--- SS14.ConfigProvider/Model/ConfigurationStore.cs
+++ SS14.ConfigProvider/Model/ConfigurationStore.cs
@@ -9,13 +9,13 @@ namespace SS14.ConfigProvider.Model;
 public sealed class ConfigurationStore
 {
     public int Id { get; init; }
-    
+
     [Required, MaxLength(255)]
     public string Name { get; init; } = string.Empty;
-    
+
     [MaxLength(2000)]
     public string? Value { get; set; }
-    
-    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-    public DateTime UpdatedOn { get; init; }
+
+    // TODO: Make this auto update?
+    public DateTime UpdatedOn { get; init; } = DateTime.UtcNow;
 }
